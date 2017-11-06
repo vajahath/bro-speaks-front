4558efe383c735b90199adc2333fc1c77d127929
Vajahath Ahmed <vajuoff.1@gmail.com>
2017-11-06 15:35:41 +0530

    blueprint for public-jokes

--- src/app/app/app.module.ts
+++ src/app/app/app.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { PublicJokesComponent } from '../public-jokes/public-jokes.component';
+
+const appRoutes: Routes = [
+	{ path: '', component: PublicJokesComponent }
+	// { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
-	imports: [BrowserModule],
-	declarations: [AppComponent],
+	imports: [
+		BrowserModule,
+		RouterModule.forRoot(appRoutes, { enableTracing: true })
+	],
+	declarations: [AppComponent, PublicJokesComponent],
 	bootstrap: [AppComponent],
 	providers: []
 })
---
+++ src/app/public-jokes/public-jokes.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-public-jokes',
+	templateUrl: './public-jokes.component.html',
+	styleUrls: ['./public-jokes.component.css']
+})
+export class PublicJokesComponent {}
