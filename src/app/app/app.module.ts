6d0aa143af3e21a5df596593e3885a3526e1b6d5
Vajahath Ahmed <vajuoff.1@gmail.com>
2017-11-06 17:05:00 +0530

    adds routing module - milestone 2 complete

--- src/app/app/app.module.ts
+++ src/app/app/app.module.ts
@@ -3,22 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
 import { PublicJokesComponent } from '../public-jokes/public-jokes.component';
 import { UserDashComponent } from '../user-dash/user-dash.component';
 import { PageNotFoundComponent } from '../page-not-found/page-not-found.component';
 
-const appRoutes: Routes = [
-	{ path: 'jokes', component: PublicJokesComponent },
-	{ path: 'u', component: UserDashComponent },
-	{ path: '', redirectTo: '/jokes', pathMatch: 'full' },
-	{ path: '**', component: PageNotFoundComponent }
-];
-
 @NgModule({
-	imports: [
-		BrowserModule,
-		RouterModule.forRoot(appRoutes, { enableTracing: true })
-	],
+	imports: [BrowserModule, AppRoutingModule],
 	declarations: [
 		AppComponent,
 		PublicJokesComponent,
