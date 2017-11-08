9e80120c366dd0f470f9efae20a7729e0795c924
Vajahath Ahmed <vajuoff.1@gmail.com>
2017-11-08 17:26:15 +0530

    adds moke jokes

---
+++ src/app/dash-home/moke-jokes.ts
@@ -0,0 +1,11 @@
+import { Joke } from './Joke';
+
+export const JOKES: Joke[] = [
+	{ author: 'billa', jokeText: 'fun joke1' },
+	{ author: 'billb', jokeText: 'fun joke2' },
+	{ author: 'billc', jokeText: 'fun joke3' },
+	{ author: 'billd', jokeText: 'fun joke4' },
+	{ author: 'bille', jokeText: 'fun joke5' },
+	{ author: 'billf', jokeText: 'fun joke6' },
+	{ author: 'billg', jokeText: 'fun joke7' }
+];
