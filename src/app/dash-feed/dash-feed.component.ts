d231f722d39bd2ff37b9e44c60ce77e18dbc3995
Vajahath Ahmed <vajuoff.1@gmail.com>
2017-11-20 18:14:14 +0530

    adds user feed option

--- src/app/dash-feed/dash-feed.component.ts
+++ src/app/dash-feed/dash-feed.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Joke } from '../Joke';
+import { JokeService } from '../joke.service';
+
+import { PublishJokeService } from '../user-dash/publish-joke.service';
+import { PublishJokeResponse } from '../user-dash/Publish-joke-response';
+
+import { MatSnackBar } from '@angular/material';
 
 @Component({
 	selector: 'app-dash-feed',
@@ -6,7 +13,37 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./dash-feed.component.css'],
 })
 export class DashFeedComponent implements OnInit {
-	constructor() {}
+	constructor(
+		private jokeService: JokeService,
+		private publishJokeService: PublishJokeService,
+		public snackBar: MatSnackBar,
+	) {}
 
-	ngOnInit() {}
+	jokes: Joke[];
+	newJoke: string;
+
+	getJokes(): void {
+		this.jokeService.getJokes().subscribe(jokes => (this.jokes = jokes));
+	}
+
+	publishJoke(newJoke: string): void {
+		const joke: Joke = {
+			jokeText: newJoke,
+			author: 'billy',
+		};
+
+		this.publishJokeService
+			.publishJoke(joke)
+			.subscribe((res: PublishJokeResponse) => {
+				this.jokes.push(joke);
+				this.snackBar.open('message', 'action', {
+					duration: 2000,
+				});
+				this.newJoke = '';
+			});
+	}
+
+	ngOnInit() {
+		this.getJokes();
+	}
 }
