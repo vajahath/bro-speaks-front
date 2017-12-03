f61ecbc92d81539972fdc0d7b5da79a7c34b8cd1
Vajahath Ahmed <vajuoff.1@gmail.com>
2017-12-03 17:57:58 +0530

    better dash-feed test

--- src/app/user-dash/dash-feed/dash-feed.component.spec.ts
+++ src/app/user-dash/dash-feed/dash-feed.component.spec.ts
@@ -2,6 +2,20 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashFeedComponent } from './dash-feed.component';
 
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { JokeCardModule } from '../../joke-card/joke-card.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { PublishJokeService } from '../publish-joke.service';
+import { JokeService } from '../../joke.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 describe('DashFeedComponent', () => {
 	let component: DashFeedComponent;
 	let fixture: ComponentFixture<DashFeedComponent>;
@@ -10,6 +24,21 @@ describe('DashFeedComponent', () => {
 		async(() => {
 			TestBed.configureTestingModule({
 				declarations: [DashFeedComponent],
+				imports: [
+					BrowserModule,
+					FormsModule,
+					BrowserAnimationsModule,
+
+					// ng-material
+					MatButtonModule,
+					MatListModule,
+					MatCardModule,
+					MatFormFieldModule,
+					MatInputModule,
+					MatSnackBarModule,
+					JokeCardModule,
+				],
+				providers: [PublishJokeService, JokeService],
 			}).compileComponents();
 		}),
 	);
