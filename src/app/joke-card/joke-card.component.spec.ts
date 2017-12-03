91e9d59f1c54fa4f0ded44e28966603c1e1794d2
Vajahath Ahmed <vajuoff.1@gmail.com>
2017-12-03 17:44:11 +0530

    makes joke-card component test

--- src/app/joke-card/joke-card.component.spec.ts
+++ src/app/joke-card/joke-card.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { JokeCardComponent } from './joke-card.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 describe('JokeCardComponent', () => {
 	let component: JokeCardComponent;
@@ -9,14 +11,19 @@ describe('JokeCardComponent', () => {
 	beforeEach(
 		async(() => {
 			TestBed.configureTestingModule({
-				declarations: [JokeCardComponent]
+				declarations: [JokeCardComponent],
+				imports: [MatButtonModule, MatCardModule],
 			}).compileComponents();
-		})
+		}),
 	);
 
 	beforeEach(() => {
 		fixture = TestBed.createComponent(JokeCardComponent);
 		component = fixture.componentInstance;
+		component.joke = {
+			author: 'someone',
+			jokeText: 'very funny',
+		};
 		fixture.detectChanges();
 	});
 
